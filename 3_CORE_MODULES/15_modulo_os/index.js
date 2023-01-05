2d0d499c1bf9da796d30203ec906688866992da2
TawanAlves <tas.tawan@.com>
2023-01-05 16:19:07 -0300

    nova branch

--- 3_CORE_MODULES/15_modulo_os/index.js
+++ 3_CORE_MODULES/15_modulo_os/index.js
@@ -7,3 +7,11 @@ console.log(os.freemem())
 console.log(os.homedir())
 
 console.log(os.type())
+
+console.log(os.cpus())
+
+console.log(os.freemem())
+
+console.log(os.homedir())
+
+console.log(os.type())
