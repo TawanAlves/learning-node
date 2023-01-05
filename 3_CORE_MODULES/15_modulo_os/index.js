ad55290df8fefd71dc69490325ebf26ca253592d
TawanAlves <tas.tawan@.com>
2023-01-05 17:35:02 -0300

    iniciando npm

--- 3_CORE_MODULES/15_modulo_os/index.js
+++ 3_CORE_MODULES/15_modulo_os/index.js
@@ -7,11 +7,3 @@ console.log(os.freemem())
 console.log(os.homedir())
 
 console.log(os.type())
-
-console.log(os.cpus())
-
-console.log(os.freemem())
-
-console.log(os.homedir())
-
-console.log(os.type())
